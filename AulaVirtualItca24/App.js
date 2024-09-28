b02b35eda98e714ad5c9cbf2bd1bac0e771801bb
Erick <Erick@CUBIAS>
2024-09-27 18:15:21 -0600

    Commit: Login, Registro y Cursos agregados. Navagacion agregada, asegurarse de instalar las siguientes dependecias *@react-navigation/native *@react-navigation/stack *react-native-screens *react-native-safe-area-context *@react-native-async-storage/async-storage *expo-linear-gradient

--- AulaVirtualItca24/App.js
+++ AulaVirtualItca24/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Login from './frontend/src/alumno/Login';
+import Registro from './frontend/src/alumno/Registro';
+import Cursos from './frontend/src/alumno/Cursos';
+
+
+
+const Stack = createStackNavigator();
+
 const App = () => {
-return <Login/>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Cursos" component={Cursos} />
+        <Stack.Screen name="Registro" component={Registro} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
+
 export default App;
