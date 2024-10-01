fd24d38ab4ab53ad9b0448c3b0dfd23292317b9c
MarvinAndrade <guayoandrade1989@gmail.com>
2024-09-30 23:18:12 -0600

    commit: se agrego material estudio para java

--- AulaVirtualItca24/App.js
+++ AulaVirtualItca24/App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from './frontend/src/alumno/Login';
 import Registro from './frontend/src/alumno/Registro';
 import Cursos from './frontend/src/alumno/Cursos';
+import MaterialEstudioJava from './frontend/src/alumno/MaterialEstudioJava';
 
 
 
@@ -16,7 +17,7 @@ const App = () => {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cursos" component={Cursos} />
         <Stack.Screen name="Registro" component={Registro} />
-        
+        <Stack.Screen name="MaterialEstudioJava" component={MaterialEstudioJava} />
       </Stack.Navigator>
     </NavigationContainer>
   );
