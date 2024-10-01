dc0e943bfe5d8a3c02752691e867a416ec75a484
JORGE ENRIQUE ALFARO LEIVA <enrique.alfaro18@itca.edu.sv>
2024-09-30 22:36:14 -0600

    Se agrego pantalla de foro

--- AulaVirtualItca24/App.js
+++ AulaVirtualItca24/App.js
@@ -4,7 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from './frontend/src/alumno/Login';
 import Registro from './frontend/src/alumno/Registro';
 import Cursos from './frontend/src/alumno/Cursos';
-
+import ForumDetailScreen from './frontend/src/alumno/ForumDetailScreen';
+import ForumsScreen from './frontend/src/alumno/ForumsScreen';
 
 
 const Stack = createStackNavigator();
@@ -16,6 +17,7 @@ const App = () => {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cursos" component={Cursos} />
         <Stack.Screen name="Registro" component={Registro} />
+        <Stack.Screen name="Foro" component={ForumsScreen} />
         
       </Stack.Navigator>
     </NavigationContainer>
